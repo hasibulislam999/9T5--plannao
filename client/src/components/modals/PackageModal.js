d7f4a0ee9024980b167c1c0424a38aab6400c4ae
hasibulislam999 <hasib143sl@gmail.com>
2023-03-08 23:58:19 +0600

    update with auth guard compensate

--- client/src/components/modals/PackageModal.js
+++ client/src/components/modals/PackageModal.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { openModal } from "../../features/auth/authSlice";
 import { closeModal } from "../../features/package/packageSlice";
 import Purchase from "../payment/Purchase";
 import Transaction from "../payment/Transaction";
 
 const PackageModal = () => {
   const { package: mentor, isOpen } = useSelector((state) => state.package);
+  const { user } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   const [option, setOption] = useState("Purchase");
 
@@ -48,11 +50,40 @@ const PackageModal = () => {
                       </svg>
                     </button>
                   </div>
-                  {/* content display here! */}
-                  {option === "Purchase" && (
-                    <Purchase setOption={setOption} mentor={mentor} />
+                  {Object.keys(user).length ? (
+                    <>
+                      {/* content display here! */}
+                      {option === "Purchase" && (
+                        <Purchase setOption={setOption} mentor={mentor} />
+                      )}
+                      {option === "Transaction" && (
+                        <Transaction mentor={mentor} />
+                      )}
+                    </>
+                  ) : (
+                    <span class="bg-red-100 text-red-800 text-xs inline-flex items-center px-2.5 py-0.5 rounded border border-red-500 font-bold gap-x-1">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                      প্রথমে
+                      <span
+                        onClick={() => dispatch(openModal())}
+                        className="underline cursor-pointer"
+                      >
+                        সাইন-ইন
+                      </span>
+                      করুন
+                    </span>
                   )}
-                  {option === "Transaction" && <Transaction mentor={mentor} />}
                 </div>
               </div>
             </div>
