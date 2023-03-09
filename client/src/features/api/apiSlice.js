1beac644a61093b861e119f82b8998bbcd00f7f0
hasibulislam999 <hasib143sl@gmail.com>
2023-03-09 10:23:27 +0600

    set base url based on environment file

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://api.plannao.com/",
+    baseUrl: process.env.REACT_APP_BASE_URL,
   }),
   tagTypes: ["Package", "User", "Transaction"],
   endpoints: () => ({}),
